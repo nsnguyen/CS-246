ec78a497c34f69897add443b5ac50a69dd6fc34f
nnguyen <nnguyen@alg.com>
2017-10-11 23:05:51 -0700

    Add ParseJSON

--- vm-shared/project1/GetWebpage.java
+++ vm-shared/project1/GetWebpage.java
@@ -3,28 +3,21 @@ import java.io.*;
 
 public class GetWebpage {
 	public static void main(String args[]) throws Exception {
-		// String builder to build the string of content which will then stream to
-		// terminal.
-		StringBuilder content = new StringBuilder();
 		String website = args[0];
 		String line;
-		
+
 		URL url = new URL(website);
 		URLConnection conn = url.openConnection();
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		
-		while(true) {
+
+		while (true) {
 			line = br.readLine();
-			if(line != null) {
-				content.append(line + "\n");
-			}
-			else {
+			if (line != null) {
+				System.out.println(line);
+			} else {
 				br.close();
 				break;
 			}
 		}
-		
-		System.out.println(content);
 	}
 }
